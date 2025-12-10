b7c3a461e2261503e922077d179674b96c845764
SchwabJust1n <github.depict091@passmail.net>
2025-12-10 15:44:52 +0100

    planner

---
+++ src/resource.ts
@@ -0,0 +1,105 @@
+import { stableStringify } from "./lib/stringify";
+import { promiseStream } from "./lib/stream";
+import { PlannerCB } from "./planner";
+
+type ContextArgs<C> = keyof C extends never ? void : C;
+type Provider<T> = (args: {
+  handler: () => Promise<T> | T;
+  planner: PlannerCB;
+}) => Promise<void>;
+
+type Instance<T> = {
+  refCount: number;
+  running: boolean;
+  close: () => void;
+  get: () => Promise<T>;
+};
+
+export class Resource<T, C = {}> {
+  private instances = new Map<string, Instance<T>>();
+
+  constructor(
+    private init: (
+      provider: Provider<T>,
+      args: ContextArgs<C>
+    ) => Promise<void> | void
+  ) {}
+
+  async use(ctx: ContextArgs<C>) {
+    const key = stableStringify(ctx);
+    const instance =
+      this.instances.get(key) ?? (await this.createInstance(key, ctx));
+
+    instance.refCount++;
+
+    return this.createHandle(instance);
+  }
+
+  private async createInstance(
+    key: string,
+    ctx: ContextArgs<C>
+  ): Promise<Instance<T>> {
+    let get!: () => Promise<T>;
+    let close!: () => void;
+    let running = true;
+
+    const provider: Provider<T> = ({ handler, planner }) => {
+      const { call, getValue } = this.createStream(handler, () => running);
+
+      const cleanup: (() => void)[] = [];
+
+      get = getValue;
+      planner(call, (fn) => cleanup.push(fn));
+
+      return new Promise<void>((resolve) => {
+        close = () => {
+          cleanup.forEach((fn) => fn());
+          this.instances.delete(key);
+          if (running) resolve();
+          running = false;
+        };
+      });
+    };
+
+    await this.init(provider, ctx);
+
+    const instance = {
+      refCount: 0,
+      running,
+      close,
+      get,
+    };
+
+    this.instances.set(key, instance);
+    return instance;
+  }
+
+  private createStream(
+    handler: () => Promise<T> | T,
+    isRunning: () => boolean
+  ) {
+    return promiseStream(
+      (async function* () {
+        while (isRunning()) {
+          const t = await handler();
+          if (!isRunning()) return;
+          yield t;
+        }
+      })()
+    );
+  }
+
+  private createHandle(instance: Instance<T>) {
+    return {
+      get value() {
+        return instance.get();
+      },
+      async [Symbol.asyncDispose]() {
+        instance.refCount--;
+        if (instance.refCount === 0) {
+          instance.close();
+        }
+      },
+    };
+  }
+}
